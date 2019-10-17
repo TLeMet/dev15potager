af0a6b3df9d2d6f15f4f4ff85a4fa1a1b11b3c82
claudio78420 <claudio.rafael@hotmail.fr>
2019-10-17 15:11:23 +0200

    Hello

--- dev15potager/src/app/app.module.ts
+++ dev15potager/src/app/app.module.ts
@@ -3,27 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { PersoComponent } from './perso/perso.component';
 import { NavbarPersoComponent } from './navbar-perso/navbar-perso.component';
-=======
 import { RecherchelistepotagersComponent } from './recherchelistepotagers/recherchelistepotagers.component';
->>>>>>> 7a5b02eec284eb38743e736a4394cb335ca02c76
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     PersoComponent,
-    NavbarPersoComponent
-=======
+    NavbarPersoComponent,
     RecherchelistepotagersComponent
->>>>>>> 7a5b02eec284eb38743e736a4394cb335ca02c76
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RecherchelistepotagersComponent
+    RecherchelistepotagersComponent,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
