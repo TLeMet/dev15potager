de3f84f1c681df04ba906128d7ae3f1726374d49
claudio78420 <claudio.rafael@hotmail.fr>
2019-10-18 16:50:09 +0200

    creation users et travail sur inscription

---
+++ dev15potager/src/app/model/User.ts
@@ -0,0 +1,20 @@
+export class User{
+    id: number;
+    nom: string;
+    prenom: string;
+    mail: string;
+    pw: string;
+    tel: string;
+    age: number;
+
+    constructor() {}
+/*
+    public get getId() : number {
+        return this.id;
+    }
+
+    public set setId(id : number) {
+        this.id = id;
+    }
+    */
+}
