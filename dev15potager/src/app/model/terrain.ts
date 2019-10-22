103820db800a988476f6d3e6d3e595b83fb3a83e
Eowuld <alex.barthes@gmail.com>
2019-10-22 16:36:34 +0200

    m

--- dev15potager/src/app/model/terrain.ts
+++ dev15potager/src/app/model/terrain.ts
@@ -1,24 +1,15 @@
 import { User } from './User';
 
-export class Terrain{
+export class Terrain {
     id: number;
-    nom: String;
-    gps: String;
-    adresse: String;
-    ville: String;
-    surface : number;
+    nom: string;
+    gps: string;
+    adresse: string;
+    ville: string;
+    surface: number;
     maxUser: number;
-    description: String;
+    description: string;
     proprietaire: User;
 
     constructor() {}
-/*
-    public get getId() : number {
-        return this.id;
-    }
-
-    public set setId(id : number) {
-        this.id = id;
-    }
-    */
 }
