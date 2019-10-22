006564196d831117c864a1a19ec8ad4ce7d51649
TLeMet <thomas@lemetayer.fr>
2019-10-22 14:17:35 +0200

    (futur) validation format modif téléphone

--- dev15potager/src/app/modifprofil/modifprofil.component.ts
+++ dev15potager/src/app/modifprofil/modifprofil.component.ts
@@ -13,11 +13,22 @@ import { SessionuserService } from '../sessionuser.service';
 export class ModifprofilComponent implements OnInit {
 
   userConnecte;
+  regexTel = new RegExp('0(6|7)\d\d\d\d\d\d\d');
 
   constructor(private http : HttpClient, private route: Router, private servi : ServiceinfouserService, private servisession: SessionuserService) { }
 
-
-
+  /*
+  checkValidTel() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (this.regexTel.test(this.userConnecte.tel)) {
+            resolve({ emailIsTaken: true })
+        } else {resolve(null)}
+      }, 2000)
+    },)
+  }
+  */
+  
   saveModifProfil() {
 
     const ins = this.http.put('http://localhost:8086/users/' + this.userConnecte.id, this.userConnecte).toPromise();
