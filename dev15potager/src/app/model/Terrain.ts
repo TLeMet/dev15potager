3ae107514b871291320c8ea86db54cbc6e562adc
VincentEsperance <vincent.esperance@wanadoo.fr>
2019-10-23 11:56:23 +0200

    affichage terrain proprio ou non 3

--- dev15potager/src/app/model/Terrain.ts
+++ dev15potager/src/app/model/Terrain.ts
@@ -1,4 +1,5 @@
 import { User } from './User';
+import { Type } from './Type';
 
 export class Terrain{
     id: number;
@@ -9,6 +10,10 @@ export class Terrain{
     maxUser: number;
     description: String;
     proprietaire: User;
+    surface: number;
+    hOuverture: String;
+    hFermeture: String;
+    type: Type;
 
     constructor() {}
 /*
