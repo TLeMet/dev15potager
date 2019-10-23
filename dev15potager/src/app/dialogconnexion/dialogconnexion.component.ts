b863b7a66fd04c01ca84800a8e94c4c51917b743
louisgarderes <louis.garderes@gmail.com>
2019-10-23 10:34:11 +0200

    23 10:34

--- dev15potager/src/app/dialogconnexion/dialogconnexion.component.ts
+++ dev15potager/src/app/dialogconnexion/dialogconnexion.component.ts
@@ -61,6 +61,8 @@ export class DialogconnexionComponent implements OnInit {
 
       const co = this.http.post('http://localhost:8086/login', this.login_attempt ).toPromise();
       console.log("toPromise réalisé.");
+      console.log(co);
+
 
       co.then(
       response => {
