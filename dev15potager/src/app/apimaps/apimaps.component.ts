ff63ad24dcdbb44e5ae9d86cc2defe29601a6abe
louisgarderes <louis.garderes@gmail.com>
2019-10-24 17:28:37 +0200

    test louis

--- dev15potager/src/app/apimaps/apimaps.component.ts
+++ dev15potager/src/app/apimaps/apimaps.component.ts
@@ -3,7 +3,6 @@ import { AgmMap, MapsAPILoader, GoogleMapsAPIWrapper } from '@agm/core';
 import { StockageterrainService } from '../stockageterrain.service';
 import { HttpClient } from '@angular/common/http';
 
-
 @Component({
   selector: 'app-apimaps',
   templateUrl: './apimaps.component.html',
@@ -26,14 +25,11 @@ export class ApimapsComponent {
     this.terrainadresse = this.stockageterrain.terrain.adresse;
     this.adressemodif = this.terrainadresse.replace(' ', '+');
 
-    
-
-
   this.http.get('https://api-adresse.data.gouv.fr/search/?q=' + this.adressemodif + "&postcode=" + this.terraincp).subscribe(response => {
     this.dataadresse = response;
     console.log(this.dataadresse);
   })
   this.lat = this.dataadresse.features.geometry.coordinates[0];
   this.lng = this.dataadresse.features.geometry.coordinates[1];
-}
+  }
 }
